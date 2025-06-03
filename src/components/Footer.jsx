f12deef19552a03a3ae140de050820a633a1e222
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-06-04 02:38:36 +0600

    added navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { FaFacebookF, FaHome, FaInstagram, FaPhoneAlt } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import { Link } from 'react-router';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+         <footer className="bg-[#B6B09F] border-t border-[#EAE4D5] shadow-xl text-base-content">
+        <div className="footer sm:footer-horizontal w-11/12 mx-auto p-5 lg:justify-around text-gray-900 my-5">
+            <nav>
+                <h6 className="footer-title">Contact</h6>
+                <div className="flex gap-2 items-center">
+                    <FaPhoneAlt size={15} />
+                    <h4>+880 1500-000000</h4>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <MdEmail size={18} />
+                    <h4>info@tutor-nexus.com</h4>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <FaHome size={18} />
+                    <h4>MUSAFIR House, Tangail-1980</h4>
+                </div>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <Link to={'/'} target="_blank" className="link link-hover">Terms and Conditions</Link>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Social</h6>
+                <div className="grid grid-flow-col gap-4 items-center">
+                <Link to={'https://www.x.com'} target="_blank">
+                    <FaXTwitter size={23} />
+                </Link>
+                <Link to={'https://www.instagram.com'} target="_blank">
+                    <FaInstagram size={24} />
+                </Link>
+                <Link to={'https://www.facebook.com'} target="_blank">
+                <FaFacebookF size={24} />
+                </Link>
+                </div>
+            </nav>
         </div>
+        <div className="bg-[#EAE4D5] border-b-5 border-[#B6B09F] py-4">
+            <p className="text-center w-11/12 mx-auto md:text-sm text-[12px] text-gray-800">Copyright © {new Date().getFullYear()} - All right reserved by Tutor Nexus</p>
+        </div>
+    </footer>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
+
+    const navLinks = (
+        <>
+            <NavLink to={'/'} className={'btn m-2 cursor-pointer bg-[#B6B09F] hover:bg-[#EAE4D5] text-black border border-gray-200 shadow-sm border-none'}>Home</NavLink>
+            <NavLink to={'/student/find-tutors'} className={'btn m-2 cursor-pointer bg-[#B6B09F] hover:bg-[#EAE4D5] text-black border border-gray-200 shadow-sm border-none'}>Find Tutors</NavLink>
+            <NavLink to={'/student/my-booked-tutors'} className={'btn m-2 cursor-pointer bg-[#B6B09F] hover:bg-[#EAE4D5] text-black border border-gray-200 shadow-sm border-none'}>My Booked Tutors</NavLink>
+            <NavLink to={'/tutor/add-tutorials'} className={'btn m-2 cursor-pointer bg-[#B6B09F] hover:bg-[#EAE4D5] text-black border border-gray-200 shadow-sm border-none'}>Add Tutorials</NavLink>
+            <NavLink to={'/tutor/my-tutorials'} className={'btn m-2 cursor-pointer bg-[#B6B09F] hover:bg-[#EAE4D5] text-black border border-gray-200 shadow-sm border-none'}>My Tutorials</NavLink>
+        </>
+    )
+
     return (
-        <div>
-            Navbar
+        <div className="bg-[#B6B09F] border-b border-[#EAE4D5] shadow-sm">
+            <div className="navbar w-11/12 mx-auto">
+                <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="text-[#EAE4D5] p-3 lg:hidden">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-8 w-8"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        {" "}
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16"
+                        />{" "}
+                    </svg>
+                    </div>
+                    <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-[#B6B09F] rounded-box z-1 mt-3 w-52 p-2 shadow"
+                    >
+                    {navLinks}
+                    </ul>
+                </div>
+                <a className="text-black font-semibold lg:text-xl md:text-md text-sm">Tutor <span className='text-white'>Nexus</span></a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+                </div>
+                <div className="navbar-end space-x-1.5 items-center">
+                    <>
+                    <Link to={'/auth/signin'} className="bg-[#EAE4D5] rounded-sm text-gray-800 text-xs md:text-base font-semibold px-2.5 py-2 md:px-4 cursor-pointer">Login</Link>
+                    <Link to={'/auth/signup'} className="bg-[#EAE4D5] rounded-sm text-gray-800 text-xs md:text-base font-semibold px-2.5 py-2 md:px-4 cursor-pointer"><button>SignUp</button></Link>
+                    </>
+                </div>
+            </div>
         </div>
     );
 };
