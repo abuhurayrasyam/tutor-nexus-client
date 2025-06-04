dcdc07a09fd013e05b72d05a85f9f35cbe47b9b8
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-06-04 15:55:27 +0600

    added loading component ui and notfound page ui

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div>
-            Loading
+        <div className="flex items-center justify-center min-h-screen">
+            <span className="loading loading-bars loading-xl"></span>
         </div>
     );
 };
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const NotFound = () => {
     return (
-        <div>
-            NotFound
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+            <h1 className="text-4xl font-bold text-red-600 mb-2">404 - Page Not Found</h1>
+            <p className="text-gray-600 mb-4">Oops! The page you’re looking for doesn’t exist.</p>
+            <Link to="/" className="bg-green-800 text-white px-4 py-2 rounded-xl font-medium">Go to Homepage</Link>
         </div>
     );
 };
