d64acdc0ba5d4a8395e8f3bfeea665dfb51647ff
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-06-04 16:45:36 +0600

    banner added on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../components/Banner';
 
 const Home = () => {
     return (
         <div>
-            Home
+            <section>
+                <Banner></Banner>
+            </section>
         </div>
     );
 };
