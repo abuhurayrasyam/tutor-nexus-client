6ccd31b796ab0da60fa9e6df1a8355e6b05ee501
abuhurayrasyam <abuhurayrasyamofficial@gmail.com>
2025-07-02 13:35:20 +0600

    banner section highet updated

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 const Banner = () => {
     return (
         <div className="hero bg-secondary">
-            <div className="hero-content flex-col lg:flex-row-reverse gap-10 md:gap-20 h-[70vh]">
+            <div className="hero-content flex-col lg:flex-row-reverse gap-10 md:gap-20 h-[55vh] lg:h-[70vh]">
                 <img className='w-80 md:w-100 lg:w-120 rounded-xl' src="https://img.freepik.com/free-photo/student-online-cute-girl-glasses-sweater-studying-computer-writing-down-notes_140725-164212.jpg?uid=R83316384&ga=GA1.1.130173900.1746345150&semt=ais_hybrid&w=740"/>
                 <div className='space-y-5'>
                     <motion.h1 animate={{color: ['#123458', '#030303'], transition: {duration: 2, repeat: Infinity}}} className="text-2xl md:text-3xl lg:text-5xl font-bold">Learn faster <br className='hidden lg:block'/>with your best<br/>language tutor.</motion.h1>
